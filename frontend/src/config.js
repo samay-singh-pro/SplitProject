7714f2942df916534db6aef9b47d215a069a9c9d
samay-singh-pro <samay.singh.pro@gmail.com>
2024-10-08 00:48:20 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || "https://splitproject.onrender.com/api/v1";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://splitproject.onrender.com/api/v1";
 
 export default BASE_URL;
